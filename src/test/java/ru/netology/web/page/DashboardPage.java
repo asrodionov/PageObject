90acca83cfa298a87c99c778757a5d6f93c56394
andrew-pahomov <andrew-pahomov@yandex.ru>
2020-07-19 21:42:19 +0300

    fix shouldReplenishFirstCard

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -4,15 +4,15 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 import lombok.val;
+import ru.netology.web.data.DataHelper;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
-    private ElementsCollection replenishButtons = $$("[data-test-id=action-deposit]");
-    private ElementsCollection cards = $$(".list__item");
+    private ElementsCollection cardsInfo = $$(".list__item [data-test-id]");
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
 
@@ -20,13 +20,13 @@ public class DashboardPage {
         heading.shouldBe(visible);
     }
 
-    public MoneyTransferPage replenishCard(int index){
-        replenishButtons.get(index).click();
+    public MoneyTransferPage replenishCard(DataHelper.CardInfo card){
+        cardsInfo.find(attribute("data-test-id",card.getTestId())).lastChild().click();
         return new MoneyTransferPage();
     }
 
-    public int getCardBalance(int index) {
-        val text = cards.get(index).text();
+    public int getCardBalance(DataHelper.CardInfo card) {
+        val text = cardsInfo.find(attribute("data-test-id",card.getTestId())).text();
         return extractBalance(text);
     }
 
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -7,108 +7,23 @@ import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
 
 class MoneyTransferTest {
 
     @Test
     void shouldReplenishFirstCard() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val verificationCode = getVerificationCodeFor(authInfo);
         val dashboardPage = verificationPage.validVerify(verificationCode);
-        int balanceCardFirst = dashboardPage.getCardBalance(0);
-        int balanceCardSecond = dashboardPage.getCardBalance(1);
-        val moneyTransferPage = dashboardPage.replenishCard(0);
+        int balanceCardFirst = dashboardPage.getCardBalance(getCardInfo(1));
+        int balanceCardSecond = dashboardPage.getCardBalance(getCardInfo(2));
+        val moneyTransferPage = dashboardPage.replenishCard(getCardInfo(1));
         int sum = 5000;
         moneyTransferPage.replenishCardAction(sum,2);
-        assertEquals (balanceCardFirst+sum, dashboardPage.getCardBalance(0));
-        assertEquals (balanceCardSecond-sum, dashboardPage.getCardBalance(1));
+        assertEquals (balanceCardFirst+sum, dashboardPage.getCardBalance(getCardInfo(1)));
+        assertEquals (balanceCardSecond-sum, dashboardPage.getCardBalance(getCardInfo(2)));
     }
-
-    @Test
-    void shouldReplenishSecondCard() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashboardPage = verificationPage.validVerify(verificationCode);
-        int balanceCardFirst = dashboardPage.getCardBalance(0);
-        int balanceCardSecond = dashboardPage.getCardBalance(1);
-        val moneyTransferPage = dashboardPage.replenishCard(1);
-        int sum = 5000;
-        moneyTransferPage.replenishCardAction(sum,1);
-        assertEquals (balanceCardFirst-sum, dashboardPage.getCardBalance(0));
-        assertEquals (balanceCardSecond+sum, dashboardPage.getCardBalance(1));
-    }
-    @Test
-    void shouldReplenishFirstCardNull() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashboardPage = verificationPage.validVerify(verificationCode);
-        int balanceCardFirst = dashboardPage.getCardBalance(0);
-        int balanceCardSecond = dashboardPage.getCardBalance(1);
-        val moneyTransferPage = dashboardPage.replenishCard(0);
-        int sum = 0;
-        moneyTransferPage.replenishCardAction(sum,2);
-        assertEquals (balanceCardFirst+sum, dashboardPage.getCardBalance(0));
-        assertEquals (balanceCardSecond-sum, dashboardPage.getCardBalance(1));
-    }
-
-    @Test
-    void shouldReplenishSecondCardNull() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashboardPage = verificationPage.validVerify(verificationCode);
-        int balanceCardFirst = dashboardPage.getCardBalance(0);
-        int balanceCardSecond = dashboardPage.getCardBalance(1);
-        val moneyTransferPage = dashboardPage.replenishCard(1);
-        int sum = 0;
-        moneyTransferPage.replenishCardAction(sum,1);
-        assertEquals (balanceCardFirst-sum, dashboardPage.getCardBalance(0));
-        assertEquals (balanceCardSecond+sum, dashboardPage.getCardBalance(1));
-    }
-
-    @Test
-    void shouldReplenishFirstCardOverLimit() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashboardPage = verificationPage.validVerify(verificationCode);
-        int balanceCardFirst = dashboardPage.getCardBalance(0);
-        int balanceCardSecond = dashboardPage.getCardBalance(1);
-        val moneyTransferPage = dashboardPage.replenishCard(0);
-        int sum = 500000;
-        moneyTransferPage.replenishCardAction(sum,2);
-        assertEquals (balanceCardFirst, dashboardPage.getCardBalance(0));
-        assertEquals (balanceCardSecond, dashboardPage.getCardBalance(1));
-    }
-
-    @Test
-    void shouldReplenishSecondCardOverLimit() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashboardPage = verificationPage.validVerify(verificationCode);
-        int balanceCardFirst = dashboardPage.getCardBalance(0);
-        int balanceCardSecond = dashboardPage.getCardBalance(1);
-        val moneyTransferPage = dashboardPage.replenishCard(1);
-        int sum = 500000;
-        moneyTransferPage.replenishCardAction(sum,1);
-        assertEquals (balanceCardFirst, dashboardPage.getCardBalance(0));
-        assertEquals (balanceCardSecond, dashboardPage.getCardBalance(1));
-    }
-
 }
